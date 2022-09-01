223c5f18c07d304e47735adeb9b3dba3ecc5d972
nicholasdzn <nicholasadorni1643@gmail.com>
2022-09-01 17:13:44 -0300

    leitura dos dados

---
+++ gameMechanics.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "gameMechanics.h"
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <string.h>
+#include <cctype>
+using namespace std;
+
+Game::Game(){
+
+};
+
+void Game::Append(string m, int c, int e, int h, int p, string g){
+    cout << c << endl;
+};
--- gameMechanics.h
+++ gameMechanics.h
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
 
+
 class Game{
     public:
-
+        Game();
+        void Append(string m, int c, int e, int h, int p, string g);
+        void Serve(int &x);
+        bool Full();
+        bool Empty();
     private:
+        struct Cards{
+            string model;
+            int curbWeight;
+            int engineSize;
+            int horsePower;
+            int price;
+            string group;
+            Cards *Next;
+        };
+
+        Cards *head;
+        Cards *tail;
 };
